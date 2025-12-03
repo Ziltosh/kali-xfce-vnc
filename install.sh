d04e032aab515bd7d151a6571caad8fa768a85da
nicolas <zeross30@gmail.com>
2025-12-03 16:44:44 +0100

    chore: update install.sh to configure Wine for Windows 11 and add required dependencies

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@
 export WINEPREFIX="/root/.wine"
 export WINEARCH="win64"
 
-# Forcer Windows 10 avant installation
-wine reg add "HKEY_CURRENT_USER\\Software\\Wine" /v Version /t REG_SZ /d "win10" /f 2>/dev/null || true
-winecfg -v win10
+# Forcer Windows 11 (meilleure compatibilité MT5 selon retours utilisateurs)
+wine reg add "HKEY_CURRENT_USER\\Software\\Wine" /v Version /t REG_SZ /d "win11" /f 2>/dev/null || true
 
-# Installer vcrun2019
+# Installer les dépendances requises (configuration GE-Proton qui fonctionne)
+winetricks -q dotnet48
+winetricks -q vcrun2015
 winetricks -q vcrun2019
+winetricks -q corefonts
 
-# Re-forcer Windows 10 après (winetricks peut le changer)
-wine reg add "HKEY_CURRENT_USER\\Software\\Wine" /v Version /t REG_SZ /d "win10" /f 2>/dev/null || true
-winecfg -v win10
+# Re-forcer Windows 11 après winetricks
+wine reg add "HKEY_CURRENT_USER\\Software\\Wine" /v Version /t REG_SZ /d "win11" /f 2>/dev/null || true
 
 # Installer MetaTester
 wine /root/.wine/drive_c/mt5tester.setup.exe /auto
